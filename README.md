261b9689336b95025da382cb39ebf8dd2e76ed78
Martin Sarsini <martin@apphancer.com>
2017-03-02 15:39:52 +0100

    Change comments

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # sannlarm-arduino
 Arduino sketch code for Sannlarm natural sunrise alarm clock
+This sketch allows for dimming using an Arduino board and Triac triggered coupled with zero-cross detector all controlled by parameters sent to Arduino's serial port 
 
 
 Requirements:
 TimerOne.h avaiable from http://www.arduino.cc/playground/Code/Timer1
+Arduino Board
+AC LED Light Dimmer Module by Krida Electronics http://www.inmojo.com/store/krida-electronics/item/ac-led-light-dimmer-module-with-heatsink/
--- sannlarm/sannlarm.ino
+++ sannlarm/sannlarm.ino
@@ -1,4 +1,3 @@
-
 /*
 AC dual dimmer Control for Sannlarm
 adapted from sketch by Robert Twomey <rtwomey@u.washington.edu>
@@ -7,8 +6,8 @@ http://wiki.dxarts.washington.edu/groups/general/wiki/4dd69/
 
 #include <TimerOne.h>
 
-volatile int i=0;               // Variable to use as a counter
-int AC_pin = 7;                 // Output to Opto Triac load#1
+volatile int i=0;
+int AC_pin = 7;                 // Output to Opto Triac
 int freqStep = 78;              // This is the delay-per-brightness step in microseconds. 65 uS for a 60Hz sinewave 
 
 String serial_value;
@@ -25,34 +24,33 @@ int dim = 128;                  // Dimming level (0-128)  0 = on, 128 = 0ff
 // 1000000 us / 100 Hz = 10000 uS, length of one half-wave.
 // 10000 uS / 128 steps = 65 uS / step
 
-void setup() {                                   
+void setup() {
   Serial.begin(9600);
   pinMode(AC_pin, OUTPUT);                        // Set the Triac pin as output
   attachInterrupt(1, zero_cross_detect, RISING);  // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
   Timer1.initialize(freqStep);                    // Initialize TimerOne library for the freq we need
   Timer1.attachInterrupt(dim_check, freqStep);
-  
-  // Use the TimerOne Library to attach an interrupt to the function we use to check to see if it is the right time to fire the triac
+
+  // Use the TimerOne Library to attach an interrupt to the function we use to check to see if it is the right time to fire the Triac
   // This function will now run every freqStep
 }
 
 void zero_cross_detect() {
   i=0;
-  // since the control pin stays high, the TRIAC won't 'unlatch' when zero-crossing so I need to put the pins to LOW
+  // since the control pin stays high, the Triac won't 'unlatch' when zero-crossing so I need to put the pins to LOW
   digitalWrite(AC_pin, LOW);
   // writing pins is like 10 times faster if we write the register directly instead of using 'digitalWrite'
 }
 
-// Turn on the TRIAC at the appropriate time
+// Turn on the Triac at the appropriate time
 void dim_check() {
     if(i>=dim) {
-      digitalWrite(AC_pin, HIGH); // turn on triac
+      digitalWrite(AC_pin, HIGH); // turn on Triac
     }
     i++; // increment time step counter
 }
 
 void loop() {
-  // here goes the code that sets the global variables dim
   if(Serial.available()) {
     serial_value = Serial.readString();
     dimming = serial_value.toInt();
